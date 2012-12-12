9cbdfd2f4ba84fb52f8cf901a37b80d1032a8aca
pdm <ppppdm@gmail.com>
2012-12-12 17:37:56 +0800

    modify busStopCalculate.py

--- busStopCalculate.py
+++ busStopCalculate.py
@@ -16,8 +16,15 @@ class bus:
 
 class stopBus:
     def __init__(self):
+        self.name=''
         self.buses=[None]*2 #buses中元素仅记录bus到站的距离
         return
+    
+    def setName(self, name):
+        self.name=name
+    
+    def getName(self, name):
+        return self.name
     
     def setBus(self, buses):
         self.buses=buses
@@ -25,30 +32,95 @@ class stopBus:
     def getBus(self):
         return self.buses
 
-def addBuses(buses, bus2V):
-    #首先对buses进行排序
+class busQueue:
+    def __init__(self, len):
+        self.queue=[None]*len
+        self.bias=0
+        self.len=len
     
-    #bus2V中两辆车,第一辆当前最早的一辆,第二辆是次早的一辆
-    return
+    def insert(self, bus):
+        self.queue[self.bias]=bus
+        self.bias+=1
+        if self.bias >= self.len:
+            self.bias=0
 
-def updateLineStopBus(lineBus):
+    
+    #N 代表第几个,第一个从1开始
+    def getNth(self, n):
+        return self.queue[(self.bias-n)%self.len]
+    
+    def setNth(self, n, v):
+        self.queue[(self.bias-n)%self.len]=v
+
+
+
+####################################################
+def getBusDist(buses, bustable):
+    dist =[]
+    for i in buses:
+        info=bustable.findById(i)
+        dist.append(info.dist)
+    return dist
+
+def addBuses(buses, bq):
+    #首先对buses进行排序,距离大的排在前面
+    buses.sort(reverse=True)
+    print(buses)
+    #将buses加入到bq中
+    for bus in buses:
+        bq.insert(bus)
+    return
+'''
+updateLineStopBus 根据之前的lineBus计算结果获取线路上每个公交站的到其的
+前两辆公交车距离该站的距离
+从线路的起始站开始,找到两辆公交车,并计算这两辆公交车到该站的距离.
+若已经有两辆公交车,又找到了一辆新的公交车,则新公交车为首辆到达的公交车
+原来首辆到达的公交车变为次到达此战的公交车.
+
+对于站点标识为'-1'的不将其加入到linestopbus中
+'''
+def updateLineStopBus(lineBus, bustable):
     mlineStopBus=[]
-    bus2V=[None]*2
+    bq=busQueue(2)
     
     for stop in lineBus:
         print(stop)
-        stop.name
-        stop.dist
-        addBuses(stop.buses, bus2V)
+        #buses中是id,通过id在bustable中找到该bus距离!!!!
+        #简化,buses中存有距离
+        busdist=getBusDist(stop.buses, bustable)
+        addBuses(busdist, bq)
+        
         if stop.name != '-1':
-            print(1)
-        else:
-            print(2)
-        print(bus2V)
-    
+            sbus=stopBus()
+            sbus.setName(stop.name)
+            bus=[bq.getNth(1), bq.getNth(2)]
+            sbus.setBus(bus)
+            mlineStopBus.append(sbus)
+
+        for v in bq.queue:
+            v+=stop.dist
     return mlineStopBus
 
 if __name__=='__main__':
     print('test')
+    print('test busQueue')
+    bq=busQueue(2)
+    bq.insert(3)
+    print(bq.getNth(1))
+    bq.insert(4)
+    print(bq.getNth(1))
+    bq.setNth(1, 9)
+    print(bq.getNth(1))
+    bq.insert(0)
+    print(bq.getNth(1))
+    
+    print('test addBuses')
+    addBuses([2, 5, 7], bq)
+    print(bq.getNth(1))
+    print(bq.getNth(2))
+    
+    
+    print('test updateLineStopBus')
+    #lsb = updateLineStopBus()
     
     print('exit')
