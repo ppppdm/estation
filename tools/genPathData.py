21d98a0d25a6b96103c63fdd52e96e7d22c4401d
pdm <ppppdm@gmail.com>
2012-12-21 17:43:33 +0800

    add file tools/genPathData.py

---
+++ tools/genPathData.py
@@ -0,0 +1,18 @@
+# -*- coding: gbk -*-
+'''
+读取lines 和 path 文件夹内的文件生成
+lines.txt linepath.txt linestop.txt
+'''
+LINE_FILE='..\lines.txt'
+LINEPATH_FILE='..\linepath.txt'
+LINESTOP_FILE='..\linestop.txt'
+
+LINES_DIR='..\..\lines'
+PATHS_DIR='..\..\paths'
+
+
+
+
+
+if __name__=='__main__':
+    print('Done')
