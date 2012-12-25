af02afcfa8b5261328bbf8cc0d416b631735a3c6
pdm <ppppdm@gmail.com>
2012-12-25 15:31:57 +0800

    change busDataServer.py. busDataServer extends threading.Thread.And use argument dofunc for handle data.Test OK

--- busDataServer.py
+++ busDataServer.py
@@ -18,18 +18,12 @@ import network
 import threading
 from utils import crc_check
 from busCalculate import busInfo
-'''
-from busCalculate import updateLineBus
-from busCalculate import lineBusTable
-from busCalculate import busInfoTable
-from lineDistance import lineDistTable
-from lineDistance import linesTable
-'''
-
 from globalValues import BUS_DATA_LEN
 from globalValues import BUS_DATA_HEAD
 from globalValues import BUS_DATA_END
 
+
+##########################################
 FILENAME='busData.txt'
 file=None
 
@@ -51,23 +45,25 @@ def write_to_file(businfo):
 def printRet(businfo):
     s=businfo.id+'\t'+str(businfo.lineName)+'\t'+str(businfo.lng)+'\t'+str(businfo.lat)
     print(s)
+###########################################
 
 '''
 判断数据完整性和校验,的正确性和数据有效性
+返回值0代表出错,1代表正确
 '''
 def check_data(data):
     if len(data)!=BUS_DATA_LEN:
         print('error data len')
-        return
+        return 0
     if data[0]!=BUS_DATA_HEAD:
         print('error data head')
-        return
+        return 0
     if data[-1]!=BUS_DATA_END:
         print('error data end')
-        return
+        return 0
     if data[-2]!=crc_check(data[1:BUS_DATA_LEN-2]):
         print('error data checksum')
-        return
+        return 0
     #公交车id判断
     #id=str(data[1:5],'gbk')
     #公交车线路判断
@@ -75,91 +71,68 @@ def check_data(data):
     stream=str(data[9:13], 'gbk')
     if stream !='上行' and stream!='下行':
         print('line stream error, should be 上行 or 下行')
-        return
+        return 0
     #公交车经纬度判断
     lng=float(str(data[13:21], 'gbk'))/600000
     if lng < 110 or lng > 120:
         print('lng %f error,out bound [110,120]'%lng)
-        return
+        return 0
     lat=float(str(data[21:29], 'gbk'))/600000
     if lat < 30 or lat >40:
         print('lat %f error,out bound [30,40]'%lat)
-        return
-    
+        return 0
+    return 1
+
+'''
+预处理公交车GPS数据为bus_info
+'''
+def pretreatData(b_data):
     bus=busInfo()
-    bus.readDataPackage(data[1:BUS_DATA_LEN-2])
+    bus.readDataPackage(b_data[1:BUS_DATA_LEN-2])
     return bus
 
-'''
-接收并预处理公交车GPS数据,不向发送发返回结果
-规定公交车GPS数据包格式如下：
-    数据头 公交车ID 所属线路 经纬度 校验和 数据尾
-    数据头：长度1字节,0x55
-    公交车ID：长度4字节，ASCII表示
-    所属线路：长度4字节，ASCII表示
-    线路上下行：长度4字节
-    经纬度：16字节，前8个字节为经度，后8个字节为纬度
-    校验和：长度1字节，公交车ID，所属线路和经纬度按位异或的结果
-    数据尾：长度1字节，0xaa
-数据总长度为27个字节
-预处理判断数据校验和是否正确
-'''
-'''
-lt=linesTable()
-lt.read_from_file('lines.txt')
-
-ldt=lineDistTable()
-ldt.read_from_file('linedist.txt')
-
-lbt=lineBusTable()
-lbt.read_from_dist_file('linedist.txt')
-
-bit=busInfoTable()
-
-def handleBusData(conn, addr):
-    print('Connected by ',  addr)
-    data=conn.recv(2048)
-    print(data)
-    ret=check_data(data)
-    if ret != None:
-        write_to_file(ret)
-        printRet(ret)
-        #updateLineBus(lineTable, lineDistTable, lineBusTable, busInfoTable, busInfo)
-        #updateLineBus(lt, ldt, lbt,bit,ret)
-    conn.close()
-    return
-'''
 class handleBusData(threading.Thread):
-    def __init__(self, conn, addr):
+    def __init__(self, conn, addr, cal_handle):
         threading.Thread.__init__(self)
         self.conn=conn
         self.addr=addr
+        self.cal_handle=cal_handle
     
     def run(self):
         print('Connected by', self.addr)
-        
-        data=self.conn.recv(2048)
-        print(data)
-        ret=check_data(data)
-        if ret!=None:
-            printRet(ret)
+        b_data=self.conn.recv(2048)
+        print(b_data)
+        r_ok=check_data(b_data)
+        if r_ok:
+            bus_info=pretreatData(b_data)
+            self.cal_handle(bus_info)
         self.conn.close()
         return
 
-def busDataServer(flag):
-    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((network.HOST_OF_BUSGPS, network.PORT_OF_BUSGPS))
-    sock.listen(1)
-    while True:
-        conn, addr=sock.accept()
-        new_t=handleBusData(conn, addr)
-        new_t.start()
-        #print(new_t)
-    return
-
 class busDataServer(threading.Thread):
-    def __init__(self, ):
+    def __init__(self, cal_handle):
+        threading.Thread.__init__(self)
+        self.cal_handle=cal_handle
+        self.running=True
+        self.thread_arr=[]
         return
+    
+    def run(self):
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((network.HOST_OF_BUSGPS, network.PORT_OF_BUSGPS))
+        sock.listen(1)
+        while self.running:
+            conn, addr=sock.accept()
+            new_t=handleBusData(conn, addr, self.cal_handle)
+            new_t.start()
+            self.thread_arr.append(new_t)
+        return
+    
+    def stop(self):
+        self.running=False
+        for t in self.thread_arr:
+            t.join()
 
 if __name__=='__main__':
-    busDataServer(0)
+    busdataserver=busDataServer(printRet)
+    busdataserver.start()
